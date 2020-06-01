b6bed47a289bbd0073ea6949c5255b427f68f2dd
Maxime Rouiller <maxime.rouiller@microsoft.com>
2020-06-01 10:13:47 -0400

    Changed schema for everyone

--- FromYmlToTOC-INDEX-MAP.ps1
+++ FromYmlToTOC-INDEX-MAP.ps1
@@ -345,7 +345,8 @@ function Format-Index-Yaml {
                     #}
                     #else
                     #{
-                    #    $twitter = $indent + 'twitter: '  + $_.twitter.Replace('https://twitter.com/', '')
+                    #    $twitter = $indent + '  - title: Twitter
+    url: '  + $_.twitter.Replace('https://twitter.com/', '')
                     #}
                     return ($uid , $name, $tagline, $image, $imageSrc, $imageAlt, $location, $display, $lat, $long | Where-Object { $_.Length -gt 0 } )-join [Environment]::NewLine
                 }
